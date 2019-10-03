0c2548940dcb6229dff27417ca2d4bf123cc5e18
yashagarwalcs17 <46733509+yashagarwalcs17@users.noreply.github.com>
2019-10-03 19:43:11 +0530

    Add files via upload

---
+++ assets/header.php
@@ -0,0 +1,12 @@
+<?php $con = mysqli_connect('localhost','root','','ec'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
