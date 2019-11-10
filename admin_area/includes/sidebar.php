fdbfe16b76ce3d3b61eb54991f4d0825efa8605f
yashagarwalcs17 <46733509+yashagarwalcs17@users.noreply.github.com>
2019-11-10 09:05:27 +0530

    Create sidebar.php

---
+++ admin_area/includes/sidebar.php
@@ -0,0 +1,37 @@
+<!-- panel panel-default sidebar-menu start -->
+<div class="panel panel-default sidebar-menu">
+	<!-- panel heading start -->
+	<div class="panel-heading">
+		<h3 class="panel-title">Product Categories</h3>
+	</div>
+	<!-- panel heading end -->
+	<div class="panel-body">
+		<ul style="text-align: left;" class="nav nav-pills nav-stacked category-menu">
+			 <li ><a href="shop.php">Jackets</a></li>
+			 <li><a href="shop.php">Accessories</a></li>
+			 <li><a href="shop.php">Shoes</a></li>
+			 <li><a href="shop.php">Coats</a></li>
+			 <li><a href="shop.php">T-Shirts</a></li>
+		</ul>
+	</div>
+</div>
+<!-- panel panel-default sidebar-menu end -->
+
+
+<!-- panel panel-default sidebar-menu start -->
+<div class="panel panel-default sidebar-menu">
+	<!-- panel heading start -->
+	<div class="panel-heading">
+		<h3 class="panel-title">Categories</h3>
+	</div>
+	<!-- panel heading end -->
+	<div class="panel-body">
+		<ul style="text-align: left;" class="nav nav-pills nav-stacked category-menu">
+			 <li ><a href="shop.php">Men</a></li>
+			 <li><a href="shop.php">Women</a></li>
+			 <li><a href="shop.php">Kids</a></li>
+			 <li><a href="shop.php">Others</a></li>
+		</ul>
+	</div>
+</div>
+<!-- panel panel-default sidebar-menu end -->
