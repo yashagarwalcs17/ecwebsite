9b79806e1ec6f694919d247dd559cb2b2275fc44
yashagarwalcs17 <46733509+yashagarwalcs17@users.noreply.github.com>
2019-11-10 09:05:06 +0530

    Create footer.php

---
+++ admin_area/includes/footer.php
@@ -0,0 +1,88 @@
+<!-- footer start -->
+<div id="footer">
+	<div class="container">
+		<div class="row">
+			<!-- col-md-3 col-sm-6 start -->
+			<div class="col-md-3 col-sm-6">
+				<h4>Pages</h4>
+				<ul>
+					<li><a href="cart.php">Shopping Cart</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
+					<li><a href="shop.php">Shop</a></li>
+					<li><a href="checkout.php">My Account</a></li>
+				</ul>
+				<hr>
+				<h4>User Section</h4>
+				<ul>
+					<li><a href="checkout.php">Login</a></li>
+					<li><a href="customer_registration.php">Register</a></li>
+				</ul>
+				<hr class="hidden-md hidden-lg hidden-sm"></hr>
+			</div>
+			<!-- col-md-3 col-sm-6 end -->
+			<!-- col-md-3 col-sm-6 start -->
+			<div class="col-md-3 col-sm-6">
+				<h4>Top Product Categories</h4>
+				<ul>
+					<li><a href="#">Jacket</a></li>
+					<li><a href="#">Accessories</a></li>
+					<li><a href="#">Shoes</a></li>
+					<li><a href="#">Coats</a></li>
+					<li><a href="#">T-Shirts</a></li>
+				</ul> 
+				<hr class="hidden-md hidden-lg">
+			</div>
+			<!-- col-md-3 col-sm-6 end -->
+			<!-- col-md-3 col-sm-6 start -->
+			<div class="col-md-3 col-sm-6">
+				<h4>Where to find us</h4>
+				<p>
+					<strong>GLA University</strong>
+					<br>Yash
+					<br>Uttar Pradesh
+					<br>9453690793
+				</p>
+				<a href="contact.php">Goto contact us page.</a>
+				<hr class="hidden-md hidden-lg">
+			</div>
+			<!-- col-md-3 col-sm-6 end-->
+			<!-- col-md-3 col-sm-6 start-->
+			<div class="col-md-3 col-sm-6">
+				<h4>Get the news</h4>
+				<p class="text-muted">Subscribe here to know about us.</p>
+				<form action="" method="post">
+					<div class="input-group">
+						<input type="text" name="email" class="form-control">
+						<span class="input-group-btn">
+							<input type="submit" class="btn btn-default" value="subscribe">
+						</span>
+					</div>					
+				</form>
+				<hr>
+				<h4>Stay in Touch</h4>
+				<p class="social">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="#"><i class="fa fa-envelope"></i></a>
+				</p>
+			</div>
+		</div>
+
+	</div>
+</div>
+<!-- footer end -->
+<!-- copyright section start -->
+<div id="copyright">
+	<div class="container">
+		<div class="col-md-6">
+			<p class="pull-left">
+				&copy; 2019 Er. Yash
+			</p>
+		</div>
+		<div class="col-md-6">
+			<p class="pull-right">Template by Yash</p>
+		</div>
+	</div>
+</div>
+<!-- copyright section end -->
